553ae7d597e39caed836928371d2473850ffa5a3
TheMixas <themixaslt@gmail.com>
2026-04-09 20:59:31 +0100

    minor comment changes

--- main.cpp
+++ main.cpp
@@ -6,22 +6,16 @@
 #include "MappedFile.h"
 
 uint16_t read_be16(const uint8_t *p) {
-    // combine p[0] and p[1] into a uint16_t, big-endian
-    // hint: the first byte needs to be shifted left by 8 bits
     uint16_t result = (p[0] << 8) | p[1];
     return result;
 }
 
 uint32_t read_be32(const uint8_t *p) {
-    // combine p[0] and p[1] into a uint16_t, big-endian
-    // hint: the first byte needs to be shifted left by 8 bits
     uint32_t result = (p[0] << 24) | (p[1] << 16) | (p[2] << 8) | (p[3]);
     return result;
 }
 
 uint64_t read_be64(const uint8_t *p) {
-    // combine p[0] and p[1] into a uint16_t, big-endian
-    // hint: the first byte needs to be shifted left by 8 bits
     uint64_t result = (uint64_t) p[0] << 56 | (uint64_t) p[1] << 48 | (uint64_t) p[2] << 40 | (uint64_t) p[3] << 32 | (
                           uint64_t) p[4] << 24 | (uint64_t) p[5] << 16 | (uint64_t) p[6] << 8 | (uint64_t) (p[7]);
     return result;
@@ -269,146 +263,4 @@ int main() {
     std::cout << "Total messages: " << total << "\n";
     std::cout << "Elapsed: " << seconds << "s\n";
     std::cout << "Throughput: " << (total / seconds / 1e6) << "M msg/sec\n";
-    //std::cout << "Throughput: " << (total / seconds / 1e6) << "M msg/sec\n";
 }
-// int main() {
-//     std::cout << "Hello, World!" << std::endl;
-//     std::uint8_t add_order_buffer[36];
-//
-//     // offset 0, length 1 — Message Type
-//     add_order_buffer[0] = 0x41;
-//
-//     // offset 1, length 2 — Stock Locate = 5
-//     add_order_buffer[1] = 0x00;
-//     add_order_buffer[2] = 0x05;
-//
-//     // offset 3, length 2 — Tracking Number = 0
-//     add_order_buffer[3] = 0x00;
-//     add_order_buffer[4] = 0x00;
-//
-//     // offset 5, length 6 — Timestamp = 0
-//     add_order_buffer[5] = 0x00;
-//     add_order_buffer[6] = 0x00;
-//     add_order_buffer[7] = 0x00;
-//     add_order_buffer[8] = 0x00;
-//     add_order_buffer[9] = 0x00;
-//     add_order_buffer[10] = 0x00;
-//
-//     // offset 11, length 8 -  Order Reference Number
-//     add_order_buffer[11] = 0x00; // 50
-//     add_order_buffer[12] = 0x00;
-//     add_order_buffer[13] = 0x00;
-//     add_order_buffer[14] = 0x00;
-//     add_order_buffer[15] = 0x00;
-//     add_order_buffer[16] = 0x00;
-//     add_order_buffer[17] = 0x00;
-//     add_order_buffer[18] = 0x32;
-//
-//     //offset 19, length 1 Buy/Sell Indicator
-//     add_order_buffer[19] = 0x42; // 'b' buy
-//
-//     //offset 20, length 4 - Shares
-//     add_order_buffer[20] = 0x00;
-//     add_order_buffer[21] = 0x00;
-//     add_order_buffer[22] = 0x00;
-//     add_order_buffer[23] = 0x32;
-//
-//     //offset 24, length 8 - stock symbol // G    O    O    G    L
-//     add_order_buffer[24] = 0x47; //G
-//     add_order_buffer[25] = 0x4F; //O
-//     add_order_buffer[26] = 0x4F; //O
-//     add_order_buffer[27] = 0x47; //G
-//     add_order_buffer[28] = 0x4C; //L
-//     add_order_buffer[29] = 0x20; //(SPACE)
-//     add_order_buffer[30] = 0x20; //(SPACE)
-//     add_order_buffer[31] = 0x20; //(SPACE)
-//
-//     //offset 32, length 4 - price as integer (PRICE4) $250.75 * 10000 = 2,507,500
-//     add_order_buffer[32] = 0x00;
-//     add_order_buffer[33] = 0x26;
-//     add_order_buffer[34] = 0x42;
-//     add_order_buffer[35] = 0xEC;
-//
-//     std::uint8_t order_delete_buffer[19];
-//     order_delete_buffer[0] = 0x44; //order delete message
-//
-//     order_delete_buffer[1] = 0x00; //locate code
-//     order_delete_buffer[2] = 0x04;
-//
-//     order_delete_buffer[3] = 0x00; //tracking num
-//     order_delete_buffer[4] = 0x01;
-//
-//     order_delete_buffer[5] = 0x00; //timestamp
-//     order_delete_buffer[6] = 0x00;
-//     order_delete_buffer[7] = 0x00;
-//     order_delete_buffer[8] = 0x00;
-//     order_delete_buffer[9] = 0x00;
-//     order_delete_buffer[10] = 0x02;
-//
-//sdasdi     order_delete_buffer[11] = 0x00; // order ref num 1
-//     order_delete_buffer[12] = 0x00;
-//     order_delete_buffer[13] = 0x00;
-//     order_delete_buffer[14] = 0x00;
-//     order_delete_buffer[15] = 0x00;
-//     order_delete_buffer[16] = 0x00;
-//     order_delete_buffer[17] = 0x00;
-//     order_delete_buffer[18] = 0x01;
-//
-//
-//     auto *msg = reinterpret_cast<const AddOrder *>(add_order_buffer);
-//
-//     std::cout << "type:    " << msg->msg_type << std::endl;
-//     std::cout << "locate:  " << read_be16((const uint8_t *) &msg->stock_locate) << std::endl;
-//     std::cout << "ref:     " << read_be64((const uint8_t *) &msg->order_ref_num) << std::endl;
-//     std::cout << "side:    " << msg->side << std::endl;
-//     std::cout << "shares:  " << read_be32((const uint8_t *) &msg->shares) << std::endl;
-//     std::cout << "stock symbol: " << std::string{msg->stock, 8} << std::endl;
-//     std::cout << "price: " << read_be32((const uint8_t *) &msg->price) << std::endl;
-//
-//     auto *delete_order_msg = reinterpret_cast<const OrderDelete *>(order_delete_buffer);
-//
-//     std::cout << " --  Reading Order Delete -- " << std::endl;
-//     std::cout << "type:    " << delete_order_msg->msg_type << std::endl;
-//     std::cout << "locate:     " << read_be16((const uint8_t *) &delete_order_msg->stock_locate) << std::endl;
-//     std::cout << "tracking num:     " << read_be16((const uint8_t *) &delete_order_msg->tracking_number) << std::endl;
-//     std::cout << "timestamp: " << read_be16((const uint8_t *) &delete_order_msg->timestamp_high) + read_be32(
-//         (const uint8_t *) &delete_order_msg->timestamp_low) << std::endl;
-//     std::cout << "order ref num: " << read_be64((const uint8_t *) &delete_order_msg->order_ref_num) << std::endl;
-//
-//     uint8_t stream[55];
-//     std::memcpy(stream, add_order_buffer, 36);
-//     std::memcpy(stream + 36, delete_order_msg, 19);
-//
-//     const uint8_t *ptr = stream;
-//     const uint8_t *end = stream + 55;
-//
-//     // based on type, figure out the message length
-//     // cast to the right struct, print the fields
-//     // advance ptr by the message length
-//     while (ptr < end) {
-//         char type = (char) *ptr;
-//         switch (type) {
-//             case 'A': {
-//                 auto add_order_message = reinterpret_cast<const AddOrder *>(ptr);
-//                 std::cout << "Received add order wit order ref num:" << read_be64(
-//                     (const uint8_t *) &add_order_message->order_ref_num) << std::endl;
-//                 ptr += sizeof(AddOrder);
-//                 break;
-//             }
-//             case 'D': {
-//                 auto delete_order_message = reinterpret_cast<const OrderDelete *>(ptr);
-//                 std::cout << "Received delete order with order ref num: " << read_be64(
-//                     (const uint8_t *) &delete_order_message->order_ref_num) << std::endl;
-//                 ptr += sizeof(OrderDelete);
-//                 break;
-//             }
-//             default: {
-//                 std::cout << "Unknown type: " << type << std::endl;
-//                 ptr += 1;
-//                 break;
-//             }
-//         }
-//     }
-//     return 0;
-// }
-
